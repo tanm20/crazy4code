88bf6df5f942609b8836aa6bb6d4bf32faa46060
tanm20 <murphytoniann@gmail.com>
2018-03-21 20:10:06 +0000

    updating code

--- quiz.js
+++ quiz.js
@@ -83,7 +83,7 @@
 
     RESULTS["trait2&trait3"] = {
         name: "High Psychopathy",
-        image: "psych.jpg",
+        image: "psych 2.jpg",
         text: "Individuals who score high on psychopathy show low levels of empathy combined with high levels of impulsivity and thrill-seeking"
     };
 
